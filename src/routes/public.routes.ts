a165e0a1a49a5a6e2bbaabd1e36d8af25941aad6
22120217 <hoanglenam0905@gmail.com>
2025-12-31 09:17:33 +0700

    feat(product): add product detail page
    - create ProductDetail page and export index
    - update public routes to include product detail

--- src/routes/public.routes.ts
+++ src/routes/public.routes.ts
@@ -3,6 +3,7 @@ import MainLayout from '~/layouts/MainLayout';
 import type { AppRouteObject } from '~/types/route.type';
 
 const Home = lazy(() => import('~/pages/User/pages/Home'));
+const ProductDetail = lazy(() => import('~/pages/User/pages/ProductDetail'));
 
 const publicRoutes: AppRouteObject[] = [
   {
@@ -13,6 +14,10 @@ const publicRoutes: AppRouteObject[] = [
         path: '',
         index: true,
         Component: Home
+      },
+      {
+        path: 'products/:id',
+        Component: ProductDetail
       }
     ]
   }
